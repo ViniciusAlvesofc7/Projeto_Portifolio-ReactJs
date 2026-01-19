0114eba67fdaf9fc306762177f75b49d61826495
ViniciusAlvesofc7 <166080498+ViniciusAlvesofc7@users.noreply.github.com>
2026-01-19 19:46:27 -0300

    .

--- portifolio/src/components/Footer.jsx
+++ portifolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='w-full h-16 bg-gray-900 flex justify-center items-center gap-6'>
         <span className='text-white text-[0.8rem]'>© 2025 Vinicius Alves. Todos os direitos reservados.</span>
-        <a href="#" className='text-cyan-600 text-[0.8rem] hover:text-white transition-all duration-300 hover:rotate-4'>Ver Código</a>
+        <a href="https://github.com/ViniciusAlvesofc7/Projeto_Portifolio-ReactJs/tree/main/portifolio" className='text-cyan-600 text-[0.8rem] hover:text-white transition-all duration-300 hover:rotate-4'>Ver Código</a>
     </footer>
   )
 }
