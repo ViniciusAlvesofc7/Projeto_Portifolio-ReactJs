df15b3b95022389e7f9c986bcf3d15eec520363a
ViniciusAlvesofc7 <166080498+ViniciusAlvesofc7@users.noreply.github.com>
2026-01-20 16:45:23 -0300

    .

--- portifolio/src/components/Certificados.jsx
+++ portifolio/src/components/Certificados.jsx
@@ -43,60 +43,52 @@ const Certificados = () => {
 
       <div ref={sectionRef} className="flex flex-wrap justify-center gap-2 p-2 w-full">
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/464468e5-fccb-49d0-8364-ccd4f976f033"
-            imgSrc={ctfReact} altText="Certificado React - DankiCode" title="Curso React Js" instituicao="DankiCode" duracao=" 4 horas" />
-        </div>
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/5f781953-bcbd-4daf-8a7a-11cb08a03bcc"
-            imgSrc={ctfJavaScript} altText="Certificado JavaScript - DankiCode" title="Curso JavaScript" instituicao="DankiCode" duracao=" 14 horas"
-          />
-        </div>
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/464468e5-fccb-49d0-8364-ccd4f976f033"
+          imgSrc={ctfReact} altText="Certificado React - DankiCode" title="Curso React Js" instituicao="DankiCode" duracao=" 4 horas" />
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/3c199897-2356-484e-9c6d-c548e1730a7e"
-            imgSrc={ctfVue} altText="Certificado Vue js - DankiCode" title="Curso Vue Js" instituicao="DankiCode" duracao=" 5 horas"
-          />
-        </div>
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/66b91974-248c-4f93-afee-c1e5e0df6ae9"
-            imgSrc={ctfJava} altText="Certificado Java Desktop - DankiCode" title="Curso Java Desktop" instituicao="DankiCode" duracao=" 9 horas"
-          />
-        </div>
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/5f781953-bcbd-4daf-8a7a-11cb08a03bcc"
+          imgSrc={ctfJavaScript} altText="Certificado JavaScript - DankiCode" title="Curso JavaScript" instituicao="DankiCode" duracao=" 14 horas"
+        />
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/43d01dff-78d7-4b30-80b3-8ce366d0ccec"
-            imgSrc={ctfFront} altText="Certificado Front-End 2.0 - DankiCode" title="Curso Front-End 2.0" instituicao="DankiCode" duracao=" 9 horas"
-          />
-        </div>
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/8b2cd6b9-88e7-42bd-bc94-d95fcb975d33"
-            imgSrc={ctfTypeScript} altText="Certificado Typescript Express - DankiCode" title="Curso Typescript Express" instituicao="DankiCode" duracao=" 2 horas"
-          />
-        </div>
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/3c199897-2356-484e-9c6d-c548e1730a7e"
+          imgSrc={ctfVue} altText="Certificado Vue js - DankiCode" title="Curso Vue Js" instituicao="DankiCode" duracao=" 5 horas"
+        />
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/e493e626-4a5c-4366-b016-d2ebd3daea69"
-            imgSrc={ctfWebMaster} altText="Certificado Webmaster Front-End - DankiCode" title="Curso Webmaster Front-End" instituicao="DankiCode" duracao=" 83 horas"
-          />
-        </div>
 
-        <div className="w-40 lg:w-44">
-          <CertificadoCards
-            link="https://cursos.dankicode.com/api/certificados/b50a4c8f-1ca0-47ff-857f-a048daed8601"
-            imgSrc={ctfLogica} altText="Certificado Logica de Programação - DankiCode" title="Curso Logica de Programação" instituicao="DankiCode" duracao=" 5 horas"
-          />
-        </div>
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/66b91974-248c-4f93-afee-c1e5e0df6ae9"
+          imgSrc={ctfJava} altText="Certificado Java Desktop - DankiCode" title="Curso Java Desktop" instituicao="DankiCode" duracao=" 9 horas"
+        />
+
+
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/43d01dff-78d7-4b30-80b3-8ce366d0ccec"
+          imgSrc={ctfFront} altText="Certificado Front-End 2.0 - DankiCode" title="Curso Front-End 2.0" instituicao="DankiCode" duracao=" 9 horas"
+        />
+
+
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/8b2cd6b9-88e7-42bd-bc94-d95fcb975d33"
+          imgSrc={ctfTypeScript} altText="Certificado Typescript Express - DankiCode" title="Curso Typescript Express" instituicao="DankiCode" duracao=" 2 horas"
+        />
+
+
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/e493e626-4a5c-4366-b016-d2ebd3daea69"
+          imgSrc={ctfWebMaster} altText="Certificado Webmaster Front-End - DankiCode" title="Curso Webmaster Front-End" instituicao="DankiCode" duracao=" 83 horas"
+        />
+
+
+        <CertificadoCards
+          link="https://cursos.dankicode.com/api/certificados/b50a4c8f-1ca0-47ff-857f-a048daed8601"
+          imgSrc={ctfLogica} altText="Certificado Logica de Programação - DankiCode" title="Curso Logica de Programação" instituicao="DankiCode" duracao=" 5 horas"
+        />
       </div>
     </div>
   )
